2a0fc3a8ac2bf4b44501d142fa68dede3afa22e3
root <root@mrnobody.mrnobody>
2026-05-24 09:21:08 +0300

    dasH

---
+++ scripting_cyber/0x00-ruby_scripting/1-hello_world_class.rb
@@ -0,0 +1,11 @@
+class HelloWorld
+    def initialize(msg="Hello, World!")
+    	&commat;massage= msg
+    end
+
+    def print_hello
+        &commat;massage
+        puts "#{massage} "
+    end
+ 
+    
