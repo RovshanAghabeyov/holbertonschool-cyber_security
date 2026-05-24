48844bb34a20dfa2105d0b0b8e0118cf526bf191
root <root@mrnobody.mrnobody>
2026-05-24 11:12:14 +0300

    dasH

---
+++ scripting_cyber/0x00-ruby_scripting/3-read_file.rb
@@ -0,0 +1,17 @@
+require 'json'
+
+def count_user_ids(path)
+  file = File.read(path)
+  data = JSON.parse(file)
+  counts = Hash.new(0)
+
+  data.each do |item|
+    counts[item["userid"]] +=1
+  end
+   
+  counts.each do |user_id, count|
+    puts "#{userid}: #{usercount}"
+  end
+end 
+
+
