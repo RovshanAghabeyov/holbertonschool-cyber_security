9e82156f1895687d76bd47ecc7a5aefc896372bf
root <root@mrnobody.mrnobody>
2026-05-24 20:17:38 +0300

    dasH

---
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -0,0 +1,13 @@
+require 'json'
+
+def merge_json_files(file1_path, file2_path)
+    file1=File.read(file1_path)
+    file2=File.read(file2_path)
+    file1_data=JSON.parse(file1)
+    file2_data=JSON.parse(file2)
+
+    merge = file1_data + file2_data
+
+    File.write(file2_path, JSON.pretty_genrate(merge))
+    puts "merged file is #{file2_data}"
+end
